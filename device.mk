5cbc55b8cb45a5dc43ba0b4be8c00c4ec4635c7d
Nolen Johnson <johnsonnolen@gmail.com>
2022-08-09 10:54:48 -0400

    sapphire: Swap to QTI USB init scripts
    Change-Id: Ibb01a6a8b9817b01a8db397cdf82a89659562d62
    Signed-off-by: Nikita Savchenko <NikitaSavc@yandex.ru>

--- device.mk
+++ device.mk
@@ -612,6 +612,13 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.usb.config=mtp,adb
 endif
 
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
+
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
