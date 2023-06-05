a4fc27c9bb981f6bd8a899a8adc647e2752d3f23
Evgenii Stepanov <eugenis@google.com>
2023-06-05 16:25:52 -0700

    sapphire: Disable Kernel MTE
    Bug: 280343521
    Test: cat /proc/cmdline | grep kasan=off
    Change-Id: I42781e8efdc4cf5a89baca1af8c3d62a4984c4f2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,6 +134,7 @@ BOARD_BOOTCONFIG := \
     androidboot.usbcontroller=4e00000.dwc3
 
 BOARD_KERNEL_CMDLINE := \
+	kasan=off \
     video=vfb:640x400,bpp=32,memsize=3072000
 
 TARGET_KERNEL_ARCH := arm64
