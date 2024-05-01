8d3e55ff7fefc6d4554320f0f68373b7654ac58e
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-05-01 16:13:51 +0200

    sapphire: Enable lazy RCU from cmdline
    Change-Id: Icdc7bfe8556d995b99ecff96a9ebe602de207bd1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,8 @@ BOARD_BOOTCONFIG := \
 
 BOARD_KERNEL_CMDLINE := \
 	kasan=off \
+	rcu_nocbs=all \
+    rcutree.enable_rcu_lazy=1 \
     video=vfb:640x400,bpp=32,memsize=3072000
 
 TARGET_KERNEL_ARCH := arm64
